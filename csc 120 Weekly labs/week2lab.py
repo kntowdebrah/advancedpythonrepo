8b7c8f32dfaa477d2a32105531a3ca565a9809f4
Kwabena Debrah <knadacademics@gmail.com>
2025-09-19 19:55:08 -0400

    updates 09/2025

---
+++ csc 120 Weekly labs/week2lab.py
@@ -0,0 +1,6 @@
+print(200)
+type('Programming is fun!')
+type(88)
+collegeName = "Assumption University"
+collegeNickName = "The Greyhounds"
+print("I attend ", collegeName, " our nickname is ", collegeNickName)
