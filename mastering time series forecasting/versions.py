46b7f88294adcce2ac02ce98a4692a8a50fa0a3c
Kwabena Debrah <knadacademics@gmail.com>
2025-09-19 19:55:20 -0400

    updates 09/2025

---
+++ mastering time series forecasting/versions.py
@@ -0,0 +1,24 @@
+#check versions
+#scipy
+import scipy 
+print('scipy: %s' % scipy.__version__)
+
+#numpy
+import numpy
+print('numpy: %s' % numpy.__version__)
+
+# matplotlib
+import matplotlib
+print('matplotlib %s' % matplotlib._version)
+
+#pandas 
+import pandas 
+print('pandas: %s' % pandas.__version__)
+
+#statsmodels
+import statsmodels 
+print('statsmodels: %s' % statsmodels.__version__)
+
+#sklearn 
+import sklearn 
+print('sklearn %s' % sklearn__version__)
